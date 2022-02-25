b4fb3c3e5423128c3ead8069df1c6d375c25790e
Ariel Pinto <arielp@goja.com>
2022-02-25 14:31:23 -0400

    inicial project

---
+++ README.md
@@ -0,0 +1,1 @@
+# courseStoreMicroservice-springBoot-React
---
+++ src/main/java/com/apd/coursestoremicro/CourseStoreMicroApplication.java
@@ -0,0 +1,13 @@
+package com.apd.coursestoremicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseStoreMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseStoreMicroApplication.class, args);
+    }
+
+}
