fe34ff7c6929b412854bb7b8ba9ee34c9f1f99ec
Ariel Pinto <arielp@goja.com>
2022-03-18 12:14:08 -0400

    Create CustomUserDetailsService class and Userprincipal and utils class for user

--- api-gateway/src/main/java/com/example/apigateway/service/UserServiceImpl.java
+++ api-gateway/src/main/java/com/example/apigateway/service/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.example.apigateway.model.User;
 import com.example.apigateway.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
