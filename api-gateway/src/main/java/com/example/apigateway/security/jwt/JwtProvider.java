091dd723da2e2686363a7b1b33b072579f02199e
Ariel Pinto <arielp@goja.com>
2022-03-21 10:58:15 -0400

    Add library jwt in pom, JWT Helper Methods, token Generation, Validation and Utils

---
+++ api-gateway/src/main/java/com/example/apigateway/security/jwt/JwtProvider.java
@@ -0,0 +1,15 @@
+package com.example.apigateway.security.jwt;
+
+import com.example.apigateway.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface JwtProvider {
+
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+}
